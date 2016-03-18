ec207c33e98e3d85af2d4dc4e880301720c6e2ee
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2016-03-18 14:31:13 +0100

    configuration for logging

--- ladon-s3-server-boot/src/main/java/de/mc/ladon/s3server/config/BeanConfig.java
+++ ladon-s3-server-boot/src/main/java/de/mc/ladon/s3server/config/BeanConfig.java
@@ -4,11 +4,14 @@
 
 package de.mc.ladon.s3server.config;
 
+import de.mc.ladon.s3server.logging.LoggingRepository;
+import de.mc.ladon.s3server.logging.PerformanceLoggingFilter;
 import de.mc.ladon.s3server.repository.api.S3Repository;
 import de.mc.ladon.s3server.repository.impl.FSRepository;
 import de.mc.ladon.s3server.servlet.S3Servlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +30,7 @@ public class BeanConfig {
     @ConditionalOnMissingBean
     @Bean
     S3Repository s3Repository() {
-        return new FSRepository(fsRepoRoot);
+        return new LoggingRepository(new FSRepository(fsRepoRoot));
     }
 
     @Bean
@@ -41,6 +44,16 @@ public class BeanConfig {
         s3Servlet.setSecurityEnabled(config.isSecurityEnabled());
         bean.setServlet(s3Servlet);
         return bean;
+    }
+
+
+    @Bean
+    FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(new PerformanceLoggingFilter());
+        filterBean.addServletNames("s3servlet");
+        filterBean.setAsyncSupported(true);
+        return filterBean;
     }
 
 
