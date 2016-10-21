1abf6bb012573ee2eb6dfcf64e8b9e90b3a351e5
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2016-10-21 12:46:03 +0200

    key gen added

---
+++ ladon-s3-server/src/main/java/de/mc/ladon/s3server/auth/AuthKeyGen.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Mind Consulting UG(haftungsbeschränkt)
+ */
+package de.mc.ladon.s3server.auth;
+
+import com.google.common.io.BaseEncoding;
+
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Generates a new KeyPair
+ * @author Ralf Ulrich 21.10.16.
+ */
+public class AuthKeyGen {
+
+
+    public static AwsKeyPair generateKeypair() throws NoSuchAlgorithmException {
+        KeyGenerator generator = KeyGenerator.getInstance(AwsSignatureVersion2.HMAC_SHA_1);
+        generator.init(120);
+        byte[] awsAccessKeyId = generator.generateKey().getEncoded();
+        generator.init(240);
+        byte[] awsSecretAccessKey = generator.generateKey().getEncoded();
+        return new AwsKeyPair(
+                BaseEncoding.base64().encode(awsSecretAccessKey),
+                BaseEncoding.base64().encode(awsAccessKeyId)
+        );
+
+    }
+
+    public static class AwsKeyPair {
+        public final String awsSecretAccessKey;
+        public final String awsAccessKeyId;
+
+        public AwsKeyPair(String awsSecretAccessKey, String awsAccessKeyId) {
+            this.awsSecretAccessKey = awsSecretAccessKey;
+            this.awsAccessKeyId = awsAccessKeyId;
+        }
+
+        @Override
+        public String toString() {
+            return "AwsKeyPair{" +
+                    "awsSecretAccessKey='" + awsSecretAccessKey + '\'' +
+                    ", awsAccessKeyId='" + awsAccessKeyId + '\'' +
+                    '}';
+        }
+    }
+}
